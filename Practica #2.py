93cd3bbe443b6d6603c2ab2308ce5b4befc9b34d
Moises-eduardo-valencia <moisesvalencia835@gmail.com>
2024-10-10 17:57:26 -0600

    Add files via upload

---
+++ Practica #2.py
@@ -0,0 +1,6 @@
+a=[1,23,44,56,78,4,23,45,8,9,5,14,66]
+v=int(input("Ingresa el valor que deseas buscar: "))
+if v in a:
+    print(v,"se encuentra en la lista")
+else:
+    print(v,"no se encuentra en la lista")
