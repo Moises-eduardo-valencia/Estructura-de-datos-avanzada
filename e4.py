9e54fdc2abdfc140be8a95e8fb58dffb9f3b67f9
Moises-eduardo-valencia <moisesvalencia835@gmail.com>
2024-10-16 12:17:10 -0600

    Delete e4.py

--- e4.py
+++
@@ -1,4 +0,0 @@
-#programa que calcula un promedio
-numeros = [float(x) for x in input("Introduce una lista de números separados por espacios: ").split()]
-promedio = sum(numeros) / len(numeros)
-print(f"El promedio es {promedio}")
