e854df699788ad94e80e8ad208d40a61d9ee766b
Moises-eduardo-valencia <moisesvalencia835@gmail.com>
2024-10-16 12:16:47 -0600

    Delete e6.py

--- e6.py
+++
@@ -1,9 +0,0 @@
-#programa numero 6
-# Solicita al usuario que ingrese un número
-numero = int(input("Introduce un número: "))
-
-# Verifica si el número es par o impar
-if numero % 2 == 0:
-    print(f"{numero} es un número par")
-else:
-    print(f"{numero} es un número impar")
