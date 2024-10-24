456abcfc14b0453894731df3b0c70972942de41f
Moises-eduardo-valencia <moisesvalencia835@gmail.com>
2024-10-24 12:41:03 -0600

    Add files via upload

---
+++ Practica #8.py
@@ -0,0 +1,9 @@
+fila=[10, 20, 30, 40, 50, 60]
+primer_elemento = fila[0]
+print(f"Primer elemento: {primer_elemento}")
+ultimo_elemento=fila[-1]
+print(f"Último elemento: {ultimo_elemento}")
+tamaño_fila=len(fila)
+print(f"Tamaño de la fila: {tamaño_fila}")
+print("Elementos de la fila:")
+print(fila)
