75f3bea7901d5630d12d0eda4b2fab760cd743f5
Moises-eduardo-valencia <moisesvalencia835@gmail.com>
2024-11-08 08:48:23 -0600

    Delete Practica #12E1.py

--- Practica #12E1.py
+++
@@ -1,9 +0,0 @@
-#Sistema de registro de estudiantes inscritos
-p=set(input("Ingresa los alumnos inscritos en Programación: ").split(","))
-r=set(input("Ingresa los alumnos inscritos en Redes: ").split(","))
-print("LISTA DE ESTUDIANTES INSCRITOS EN AMBOS CURSOS")
-print(p&r)
-print("LISTA DE ESTUDIANTES INSCRITOS EN PROGRAMACION")
-print(p)
-print("LISTA DE ESTUDIANTES INSCRITOS EN REDES")
-print(r)
